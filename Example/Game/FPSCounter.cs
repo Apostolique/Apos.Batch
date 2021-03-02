90f22d8761699f968646abb64884e7f6f2dd923d
Jean-David Moisan <vynecompiler@gmail.com>
2021-03-02 15:25:59 -0500

    New FPSCounter

--- Example/Game/FPSCounter.cs
+++ Example/Game/FPSCounter.cs
@@ -1,50 +1,69 @@
 using System;
 using Apos.Input;
+using Microsoft.Xna.Framework;
 
 namespace GameProject {
     class FPSCounter {
-        public int FramesPerSecond { get; private set; } = 0;
+        public FPSCounter() {
+            TargetElapsedTime = TimeSpan.FromTicks(166667);
+        }
+
+        public TimeSpan TargetElapsedTime {
+            get => _targetElapsedTime;
+            set {
+                _targetElapsedTime = value;
+                _targetFPS = (int)Math.Round((double)ONE / _targetElapsedTime.Ticks);
+            }
+        }
         public int UpdatePerSecond { get; private set; } = 0;
-        public double TimePerFrame { get; private set; } = 0;
+        public int FramesPerSecond { get; private set; } = 0;
         public double TimePerUpdate { get; private set; } = 0;
+        public double TimePerFrame { get; private set; } = 0;
         public int DroppedFrames { get; set; } = 0;
 
-        public void Update(long elapsedTickTime) {
+        public void Update(GameTime gameTime) {
             _updateCounter++;
 
-            _totalTime += elapsedTickTime;
-            timer += elapsedTickTime;
-            if (timer <= TimeSpan.TicksPerSecond) {
-                return;
-            } else if (timer > TimeSpan.TicksPerSecond * 60) {
-                // This fixes a case where the game stops being updated for a long time.
-                // For example when the computer is in sleep mode.
-                timer = TimeSpan.TicksPerSecond * 60;
+            if (gameTime.TotalGameTime.Ticks - _lastUpdateTick < ONE) {
                 return;
             }
 
             UpdatePerSecond = _updateCounter;
-            FramesPerSecond = _framesCounter;
             _updateCounter = 0;
-            _framesCounter = 0;
-            timer -= TimeSpan.TicksPerSecond;
+            _lastUpdateTick = gameTime.TotalGameTime.Ticks;
 
             TimePerUpdate = Math.Truncate(1000d / UpdatePerSecond * 10000) / 10000;
-            if (FramesPerSecond > 0) {
-                TimePerFrame = Math.Truncate(1000d / FramesPerSecond * 10000) / 10000;
-            }
-
-            if (FramesPerSecond < 60 && _totalTime > 3000 && InputHelper.IsActive) {
-                DroppedFrames += 60 - FramesPerSecond;
-            }
-        }
-        public void Draw() {
-            _framesCounter++;
         }
 
-        private long timer = 0;
-        private int _framesCounter = 0;
+        public void Draw(GameTime gameTime) {
+            _frameCounter++;
+            long currentTick = gameTime.TotalGameTime.Ticks;
+
+            if (currentTick - _lastDrawTick < ONE) {
+                return;
+            }
+
+            FramesPerSecond = _frameCounter;
+            _frameCounter = 0;
+            _lastDrawTick = currentTick;
+
+            TimePerFrame = Math.Truncate(1000d / FramesPerSecond * 10000) / 10000;
+
+            if (FramesPerSecond < _targetFPS && currentTick > THREE && InputHelper.IsActive) {
+                DroppedFrames += _targetFPS - FramesPerSecond;
+            }
+        }
+
+        private TimeSpan _targetElapsedTime;
+        private int _targetFPS;
+
+        private long _lastUpdateTick = 0;
+        private long _lastDrawTick = 0;
+        private int _frameCounter = 0;
         private int _updateCounter = 0;
-        private long _totalTime = 0;
+
+        private const long ONE = TimeSpan.TicksPerSecond;
+        private const long THREE = TimeSpan.TicksPerSecond * 3;
+        private const long SIXTY = TimeSpan.TicksPerSecond * 60;
     }
 }
