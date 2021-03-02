e50828ac1823d669a92f10e9ec6c08cdfa39902e
Jean-David Moisan <vynecompiler@gmail.com>
2021-03-02 17:16:13 -0500

    Uncap frames, use custom shader, etc

--- Example/Game/GameRoot.cs
+++ Example/Game/GameRoot.cs
@@ -20,6 +20,10 @@ namespace GameProject {
         protected override void Initialize() {
             Window.AllowUserResizing = true;
 
+            IsFixedTimeStep = false;
+            _graphics.SynchronizeWithVerticalRetrace = false;
+            _graphics.ApplyChanges();
+
             base.Initialize();
         }
 
@@ -29,42 +33,46 @@ namespace GameProject {
             _apos = Content.Load<Texture2D>("apos");
             _s = new SpriteBatch(GraphicsDevice);
             _effect = Content.Load<Effect>("batch");
+            _custom = Content.Load<Effect>("custom");
             _b = new Batch(GraphicsDevice, _effect);
 
             _random = new Random();
-
             _fps = new FPSCounter();
 
             _fontSystem = FontSystemFactory.Create(GraphicsDevice, 2048, 2048);
             _fontSystem.AddFont(TitleContainer.OpenStream($"{Content.RootDirectory}/source-code-pro-medium.ttf"));
-
-            _font = _fontSystem.GetFont(30);
+            _font = _fontSystem.GetFont(24);
         }
 
         protected override void Update(GameTime gameTime) {
+            _fps.Update(gameTime);
             InputHelper.UpdateSetup();
-            _fps.Update(gameTime.ElapsedGameTime.Ticks);
 
             if (_quit.Pressed())
                 Exit();
-
-            // TODO: Add your update logic here
 
             InputHelper.UpdateCleanup();
             base.Update(gameTime);
         }
 
         protected override void Draw(GameTime gameTime) {
-            _fps.Draw();
+            _fps.Draw(gameTime);
             GraphicsDevice.Clear(Color.Black);
 
-            _b.Begin();
+            _b.Begin(effect: _custom);
             for (int i = 0; i < 10000; i++) {
-                _b.Draw(_apos, Num.Matrix3x2.CreateTranslation(_random.Next(0, Window.ClientBounds.Width), _random.Next(0, Window.ClientBounds.Height)));
+                _b.Draw(_apos, Num.Matrix3x2.CreateTranslation(_random.Next(0, GraphicsDevice.PresentationParameters.BackBufferWidth - 200), _random.Next(0, GraphicsDevice.PresentationParameters.BackBufferHeight - 200)));
             }
             _b.End();
+            // _s.Begin();
+            // for (int i = 0; i < 1000; i++) {
+            //     _s.Draw(_apos, new Rectangle(_random.Next(0, GraphicsDevice.Viewport.Width - 200), _random.Next(0, GraphicsDevice.Viewport.Height - 200), _apos.Width, _apos.Height), Color.White);
+            // }
+            // _s.End();
 
-            string fps = $"fps: {_fps.FramesPerSecond} - Dropped Frames: {_fps.DroppedFrames} - Draw ms: {_fps.TimePerFrame} - Update ms: {_fps.TimePerUpdate}";
+            string gc = $"{GC.CollectionCount(0)} - {GC.CollectionCount(1)} - {GC.CollectionCount(2)}";
+
+            string fps = $"fps: {_fps.FramesPerSecond} - Dropped Frames: {_fps.DroppedFrames} - Draw ms: {_fps.TimePerFrame} - Update ms: {_fps.TimePerUpdate} - {gc}";
             Vector2 size = _font.MeasureString(fps, new Vector2(1));
             _s.Begin();
             _s.FillRectangle(new RectangleF(new Vector2(0), size + new Vector2(20)), Color.Black * 0.7f);
@@ -87,6 +95,7 @@ namespace GameProject {
 
         Texture2D _apos;
         Effect _effect;
+        Effect _custom;
 
         FPSCounter _fps;
 
